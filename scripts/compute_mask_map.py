d5222ae54ee62a2ab49cfefa00775443e7a45288
fleroux <francois.leroux.pro@gmail.com>
2026-09-22 09:59:44 +0200

    save

--- scripts/compute_mask_map.py
+++ scripts/compute_mask_map.py
@@ -14,5 +14,19 @@ fig_dir = config.root_dir / "outputs"
 
 # %% parameters
 
-hwp_pixel_pitch = 0.5e-6  # [m]
+hwp_pixel_pitch = 5e-6  # [m]
 grey_width = 1.0  # [m]
+mask_extent_x = 1e-2  # [m]
+mask_extent_y = 0.5e-2  # [m]
+
+# %% compute fast axis orientatins map
+
+mask = np.ones(
+    (int(mask_extent_y / hwp_pixel_pitch), int(mask_extent_x / hwp_pixel_pitch))
+)
+
+# %% visualize the mask map
+
+plt.figure(figsize=(3.45, 0.7 * 3.45))
+plt.imshow(mask, cmap="gray")
+plt.axis("equal")
